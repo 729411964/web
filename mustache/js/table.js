e1609e44953d598f9eec6b1787f9e0c43d1122eb
wing <729411964@qq.com>
2017-03-17 03:39:21 +0800

    表格组件设计
    。。

--- mustache/js/table.js
+++ mustache/js/table.js
@@ -48,11 +48,24 @@ Cutable.tableOptions={
 //保存表格结构的列表
 (function($){
   //表格结构列表
-  var tableDivList={};
+  var tableDivList={
+    table1:'    <div class="table1-div" >'+
+             '      <div class="table-title">查询结果</div>'+
+             '      <div class="operation-div"></div>'+
+             '      <table class="table1" tableId="wing"><thead></thead><tbody></tbody></table>'+
+            '    </div>'
+            ,
+
+    table2:'    <div class="table2-div">'+
+                     '      <div class="table-title">查询结果2</div>'+
+                     '      <div class="operation-div"></div>'+
+                     '      <table class="table1" tableId="wing"><thead></thead><tbody></tbody></table>'+
+                    '    </div>'
+  };
 
   //根据 tableDivName获得表格结构
   Cutable.getTableDiv=function(tableDivName){
-    if(tableDivName &&typeof tableDivName ==="string"){
+    if(tableDivName &&typeof tableDivName ==="string"){ //此处校验可以写为公共方法
       return tableDivList[tableDivName];
     }else{
         console.error("获取表格结构时，有误");
@@ -66,20 +79,32 @@ Cutable.prototype.initTable = function () {
   $("cutable").each(function(index,item){
     var $table=$(this);
     var options=$table.data("options");
-    console.log(options);
-
+    var tableDiv=Cutable.getTableDiv(options["tableDiv"]);
+    tableDiv=tableDiv.replace("wing",options.tableId);
+    //将加载出来的模板插入页面中
+    $table.after(tableDiv);
     //清除自定义标签
     $table.remove();
-
+    //初始化表头
+    Cutable.prototype.initHeader(options.tableId,options.columns);
+    //初始化渲染数据的模板
+    Cutable.prototype.initTemplate(options.tableId,options.columns);
+    //监听事件，加载表格
+    Event.listen("xxx",function(){
+      Cutable.prototype.refreshTable(tableId);
+    })
   });
 
 };
-//通过配置表头生成模板
-Cutable.prototype.initTemplate=function(tableId){
+//通过配置生成模板
+Cutable.prototype.initTemplate=function(tableId,columns){
 
 }
 //生成表头
-Cutable.prototype.initHeader=function(tableId){
+Cutable.prototype.initHeader=function(tableId,columns){
+  var $table=$("table[tableid='"+tableId+"']");
+  var s=Mustache.render("<tr>{{#.}}<th field='{{field}}'>{{title}}</th>{{/.}}</tr>",columns);
+  $table.find("thead").html(s);
 
 }
 //刷新表格
@@ -88,5 +113,9 @@ Cutable.prototype.refreshTable=function(tableId){
 }
 //提示语
 Cutable.prototype.getTips=function(tableId){
+
+}
+//为表格添加按钮
+Cutable.prototype.addButton=function(tableId,fn){
 
 }
