3dc3b38e8e37c95b167b127a0a84314d6077346a
wing <729411964@qq.com>
2017-11-24 21:12:21 +0800

    提交 ES6相关

---
+++ ES6/dev/js/ES6-DEMO.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var a = 12;
+var b = 13;
+var c = 34;
+console.log("4578990009797979797979");
+console.log("544545dsd");
---
+++ ES6/src/js/ES6-DEMO.js
@@ -0,0 +1,6 @@
+let a = 12;
+let b = 13;
+let c =34 ;
+console.log("4578990009797979797979");
+console.log("544545dsd")
+
